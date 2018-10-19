b69feb601762f09b5c406c3f7a536e2d020691c6
Nick Gashkov <nick@gashkov.com>
2018-10-19 14:42:59 +0300

    Add 'prettier' --amend

--- yars/apps/frontend/src/components/App.js
+++ yars/apps/frontend/src/components/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 
-
 class App extends React.Component {
   render() {
-    return <h1>App</h1>
+    return <h1>App</h1>;
   }
 }
 
--- yars/apps/frontend/src/index.js
+++ yars/apps/frontend/src/index.js
@@ -2,5 +2,4 @@ import App from "./components/App";
 import React from "react";
 import ReactDOM from "react-dom";
 
-
 ReactDOM.render(<App />, document.getElementById("app"));
